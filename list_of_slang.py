77bd92ce811822b083d86981c57d01d9c5b4d003
T H <hutianyou@live.cn>
2016-07-14 12:24:35 +1200

    create list_of_slang.py

---
+++ list_of_slang.py
@@ -0,0 +1,18 @@
+"""
+Author: TH
+Date: 14/07/2016
+Collect all the slang from http://www.noslang.com/dictionary/
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd 
+
+page = requests.get("http://www.noslang.com/dictionary/a/")
+soup = BeautifulSoup(page.content, "html.parser")
+#print(soup)
+table = soup.find_all("table")[1]
+#print(table)
+dts = soup.find_all("dt")
+print(dts)
+
