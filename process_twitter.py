3ed8cbf766f87b3623dd7f4d4ed574374563b3fb
T H <hutianyou@live.cn>
2016-07-25 22:25:48 +1200

    create .gitignore and update process_twitter.py

--- process_twitter.py
+++ process_twitter.py
@@ -6,7 +6,10 @@ import re
 from nltk.tokenize import StanfordTokenizer 
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
+from nltk.stem import PorterStemmer
 import string
+
+
 def pre_process_tweet(tweet):
     """ pre-process the tweets:
         a)replace all the emoticons with a their sentiment po- larity by looking up the emoticon dictionary
@@ -15,17 +18,19 @@ def pre_process_tweet(tweet):
         d)replace targets (e.g. “@John”) with tag ||T|| 
         e)replace a sequence of repeated characters by three characters, for example, convert coooooooool to coool. 
         we add the f) step
-        f)replace #hashtag and $cashtag with tag ||T||
+        f)replace $cashtag with tag ||T|| and remove # for hashtags
     """
     # step a to be continued
     pre_process_a = tweet
     print(tweet)
-    #print(re.search(r"(?P<url>https?://[^\s]+)", pre_process_a).group("url"))
-    pre_process_b = re.sub(r"(?P<url>https?://[^\s]+)", "||U||", pre_process_a)    
+    #print(re.search(r"(?P<url>https?://[^\s]+)", pre_process_a).group("url"))   
+    pre_process_b = re.sub(r"((www\.[^\s]+)|(https?://[^\s]+))", "||U||", pre_process_a)    
     pre_process_c = re.sub(r"\s[N|n][O|o][T|t]|\s[N|n][O|o]\s|\snever\s|\sNEVER\s|[N|n]'[T|t]|cannot|CANNOT", " NOT ", pre_process_b)
     pre_process_d = re.sub(r'@[^\s]+', "||T||", pre_process_c)
     pre_process_e = re.sub(r"([a-z|A-Z])\1{3,20}", r'\1\1\1', pre_process_d)
-    pre_process_f = re.sub(r"(#[^\s]+)|(\$[^\s]+)","||T||",pre_process_e)
+    #pre_process_f = re.sub(r"(#[^\s]+)|(\$[^\s]+)","||T||",pre_process_e)
+    pre_process_f = re.sub(r"(\$[^\s\d]+)","||T||",pre_process_e)
+    pre_process_f = re.sub(r"#([^\s]+)",lambda pat: pat.group(1).lower() ,pre_process_f)
     print(pre_process_f)
     return pre_process_f
 def process_tweet(tweet):
@@ -35,11 +40,13 @@ def process_tweet(tweet):
         c) English words in WordNet(Fell- baum, 1998)
         d) identify punctuation (Penn Treebank)
     """
+    ps = PorterStemmer()
     #lists to save stop words, english words, punctuation marks and captalized words.
     stop_words_in_tweet = []
     english_words_in_tweet = []
     punctuation_marks_in_tweet = []
     capitalized_words_in_tweet = []
+    featureVector = []
     num_twitter_tags = 0
     num_exclamation_marks = 0
     num_negations = 0
@@ -89,14 +96,22 @@ def process_tweet(tweet):
             if item not in seen:
                 seen.add(item)
                 other_tokens_in_tweet.append(item)
-
-
+                
+    for w in english_words_in_tweet + other_tokens_in_tweet:
+        #check if the word stats with an alphabet
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        if val is None:
+            continue
+        else:
+            featureVector.append(ps.stem(w.lower()))
+                
+    
     print("stop_words_in_tweet",stop_words_in_tweet)
     print("english_words_in_tweet",english_words_in_tweet)
     print("punctuation_marks_in_tweet",punctuation_marks_in_tweet)
     print("capitalized_words_in_tweet",capitalized_words_in_tweet)
     print("other_tokens_in_tweet",other_tokens_in_tweet)
-
+    print("featureVector", featureVector)
     print(num_twitter_tags)
     print(num_exclamation_marks)
     print(num_negations)
@@ -105,9 +120,10 @@ def process_tweet(tweet):
 
     
 if __name__ == "__main__":
+
     Tweet1 = "Apple's new Swift Playgrounds for iPad is a killer app for teaching code #WWDC $AAPL http://appleinsider.com/articles/16/06/22/apples-new-swift-playgrounds-for-ipad-is-a-killer-app-for-teaching-code …pic.twitter.com/UW8JzzSQrS"
     Tweet2 = "@AustenAllred I actually closed it around $97 down from $120ish so nay bad but I would suspect $AAPL is going lower."
     Tweet3 = " CANNOT noise Not aa NEVER CANNOT heihei"
-    Tweet4 = "#Apple #iPhone. NOt Not no {cannot} cooln't Good slowdown shows shows [shows]  up, on @Jabil bottom line, http://bizj.us/1myby0 $AAPL $JBL"
-    pre_process_result = pre_process_tweet(Tweet4)
+    Tweet4 = "#ApPLe #iPhone. NOt Not no {cannot} cooln't Good slowdown SHOWS shows shows [shows]  up, on @Jabil bottom line, http://bizj.us/1myby0 $AAPL $JBL $155m"
+    pre_process_result = pre_process_tweet("just had some bloodwork done. my arm hurts")
     process_tweet(pre_process_result)
